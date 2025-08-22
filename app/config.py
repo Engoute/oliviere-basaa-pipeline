17c1b0a45ba7519c8d6a6d92aa1cbe9e335849b7
Engoute <64826422+Engoute@users.noreply.github.com>
2025-08-22 11:26:08 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,10 @@ from dataclasses import dataclass
 class Settings:
     host: str = os.getenv("HOST", "0.0.0.0")
     port: int = int(os.getenv("PORT", "7860"))
-    path_whisper: str = os.getenv("PATH_WHISPER")  # /data/models/whisper_hf
+
+    path_whisper: str = os.getenv("PATH_WHISPER")
+    path_m2m: str = os.getenv("PATH_M2M")  # NEW
+
     in_pcm_sr: int = 16000
 
 S = Settings()
