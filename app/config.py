c6778d70190892cde76015b93b4a076798b320cc
Engoute <64826422+Engoute@users.noreply.github.com>
2025-08-22 15:24:52 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,8 +8,10 @@ class Settings:
     port: int = int(os.getenv("PORT", "7860"))
 
     path_whisper: str = os.getenv("PATH_WHISPER")
-    path_m2m: str = os.getenv("PATH_M2M")  # NEW
+    path_m2m: str = os.getenv("PATH_M2M")
+    path_orpheus: str = os.getenv("PATH_ORPHEUS")  # NEW
 
     in_pcm_sr: int = 16000
+    tts_sr: int = int(os.getenv("TTS_SR", "24000"))
 
 S = Settings()
